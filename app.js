1b3c976b035a303000824544ad149520b8cb5521
Brittany <stachkub@oregonstate.edu>
2024-05-21 22:26:15 -0400

    Files needed from part 3 for node.js

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ app.get('/', function(req, res)
         });
     });
 
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
 /*
     ROUTES
 */
@@ -51,6 +59,13 @@ app.get('/', function(req, res)                 // This is the basic syntax for 
     {
         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
     });                                         // requesting the web site.
+
+// app.js
+
+app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
