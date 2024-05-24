785a6d9f75a4adb1fac259c3e5c3b3e76eadce38
Ally Karlis <allykarlis@gmail.com>
2024-05-23 17:06:36 -0700

    cleaned up comments

--- app.js
+++ app.js
@@ -5,77 +5,58 @@
 // Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
 // App.js
-var express = require('express');   // We are using the express library for the web server
+var express = require('express');
 var app     = express();
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 PORT        = 9987; 
+
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+var exphbs = require('express-handlebars');     
+app.engine('.hbs', engine({extname: ".hbs"}));  
+app.set('view engine', '.hbs'); 
 
 
+var db = require('./database/db-connector')
 
-// app.js
 
+// display travelers
 app.get('/', function(req, res)
     {  
-        let query1 = "SELECT * FROM Travelers;";               // Define our query
+        let query1 = "SELECT * FROM Travelers;";               
 
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+        db.pool.query(query1, function(error, rows, fields){    
 
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
-        })                                                      // an object where 'data' is equal to the 'rows' we
-    });                                                         // received back from the query
+            res.render('index', {data: rows});                  
+        })                                                      
+    });                                                         
 
 
-// app.js - ROUTES section
-
+// add traveler
 app.post('/add-traveler-ajax', function(req, res) 
 {
-    // Capture the incoming data and parse it back to a JS object
     let data = req.body;
 
-    // Capture NULL values
-    let email = parseInt(data.email);
-    if (isNaN(email))
-    {
-        email = 'NULL'
-    }
-
-    let phone_number = parseInt(data.phone_number);
-    if (isNaN(phone_number))
-    {
-        phone_number = 'NULL'
-    }
-
-    // Create the query and run it on the database
-    query1 = `INSERT INTO Travelers (first_name, last_name, email, phone_number) VALUES ('${data.first_name}', '${data.last_name}', ${email}, ${phone_number})`;
+    query1 = `INSERT INTO Travelers (first_name, last_name, email, phone_number) VALUES ('${data.first_name}', '${data.last_name}', '${data.email}', '${data.phone_number}')`;
     db.pool.query(query1, function(error, rows, fields){
 
-        // Check to see if there was an error
         if (error) {
 
-            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error)
             res.sendStatus(400);
         }
         else
         {
-            // If there was no error, perform a SELECT * on bsg_people
+            // If there was no error
             query2 = `SELECT * FROM Travelers;`;
             db.pool.query(query2, function(error, rows, fields){
-
-                // If there was an error on the second query, send a 400
                 if (error) {
                     
-                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
                     console.log(error);
                     res.sendStatus(400);
                 }
-                // If all went well, send the results of the query back.
                 else
                 {
                     res.send(rows);
@@ -86,9 +67,11 @@ app.post('/add-traveler-ajax', function(req, res)
 });
 
 
+
+// delete traveler
 app.delete('/delete-traveler-ajax/', function(req,res,next){
     let data = req.body;
-    let travelerID = parseInt(data.id);
+    let traveler_id = parseInt(data.id);
     let deleteTraveler= `DELETE FROM Travelers WHERE id = ?`;
   
   
@@ -103,6 +86,9 @@ app.delete('/delete-traveler-ajax/', function(req,res,next){
   })});
 
 
+
+
+  // update traveler
   app.put('/put-person-ajax', function(req,res,next){
   let data = req.body;
 
@@ -139,46 +125,9 @@ app.delete('/delete-traveler-ajax/', function(req,res,next){
             }
 })});
 
-// Database
-var db = require('./database/db-connector')
-
-// app.js 
-
-app.get('/', function(req, res)
-
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for karlisa!")'; //replace with your onid
-        query4 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    }); 
-
-
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){           
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
--- js/update_traveler.js
+++ js/update_traveler.js
@@ -4,13 +4,10 @@
 // Code was copied and pasted from the code snippets and changed to fit the database needs for project
 // Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-// Get the objects we need to modify
 let updateTravelerForm = document.getElementById('update-traveler-form-ajax');
 
-// Modify the objects we need
 updateTravelerForm.addEventListener("submit", function (e) {
    
-    // Prevent the form from submitting
     e.preventDefault();
 
     // Get form fields we need to get data from
@@ -18,28 +15,23 @@ updateTravelerForm.addEventListener("submit", function (e) {
     let inputEmail = document.getElementById("update-email");
     let inputNumber = document.getElementById("update-number");
 
-    // Get the values from the form fields
     let fullNameValue = inputFullName.value;
-    let email = inputEamil.value;
-    let number = inputNumber.value;
-
-    // Put our data we want to send in a javascript object
+    let email = inputEmail.value;
+    let phone_number = inputNumber.value;
+t
     let data = {
         fullname: fullNameValue,
         email: emailValue,
-        number: numberValue
+        phone_number: numberValue
     }
     
-    // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
     xhttp.open("PUT", "/put-traveler-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-            // Add the new data to the table
             updateRow(xhttp.response, fullNameValue);
 
         }
@@ -48,7 +40,6 @@ updateTravelerForm.addEventListener("submit", function (e) {
         }
     }
 
-    // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 
 })
@@ -60,17 +51,13 @@ function updateRow(data, traveler_id){
     let table = document.getElementById("travelers-table");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
-       //iterate through rows
-       //rows would be accessed using the "row" variable assigned in the for loop
+
        if (table.rows[i].getAttribute("data-value") == traveler_id) {
 
-            // Get the location of the row where we found the matching person ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
-            // Get td of homeworld value
             let td = updateRowIndex.getElementsByTagName("td")[3];
 
-            // Reassign homeworld to our value we updated to
             td.innerHTML = parsedData[0].name; 
        }
     }
